c04924aa2208e545abb6c99eca9c3cd0cd49c779
Student 6 <Student 6@10.8.33.105>
2017-02-03 12:57:08 -0500

    added new gui.jar and ShopTillYouDrop stuff

--- ShopTilYouDrop/src/main/ShopTilYouDrop.java
+++ ShopTilYouDrop/src/main/ShopTilYouDrop.java
@@ -3,7 +3,10 @@
  */
 package main;
 
-import gui.practice.GUIApplication;
+import frontEnd.IramWorkerScreen;
+import frontEnd.SammyajitCustomerScreen;
+import gui6.GUIApplication;
+import frontEnd.MatthewMain;
 
 /**
  * @author Vicki
@@ -14,21 +17,29 @@ public class ShopTilYouDrop extends GUIApplication {
 	 * static fields
 	 */
 	public static ShopTilYouDrop game;
-	public ShopTilYouDrop() {
+	public static MatthewMain main;
+	public static IramWorkerScreen worker;
+	public static SammyajitCustomerScreen customer;
+	
+	
+	public ShopTilYouDrop(int width,int height) {
+		super(width,height);
 	}
 	
-	@Override
-	protected void initScreen() {
-		// TODO Auto-generated method stub
-
-	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		game = new ShopTilYouDrop();
+		game = new ShopTilYouDrop(800,700);
 		Thread go = new Thread(game);
+	}
+
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
