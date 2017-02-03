bc8b1194112c0ccae4786be91b6eebbe3d527221
Student6 <Student6@10.8.33.107>
2017-02-03 13:01:42 -0500

    update

--- ShopTilYouDrop/src/main/ShopTilYouDrop.java
+++ ShopTilYouDrop/src/main/ShopTilYouDrop.java
@@ -18,23 +18,14 @@ public class ShopTilYouDrop extends GUIApplication {
 	 * static fields
 	 */
 	public static ShopTilYouDrop game;
-	
-	public ShopTilYouDrop() {
-		
-	}
 	public static MatthewMain main;
 	public static IramWorkerScreen worker;
 	public static SammyajitCustomerScreen customer;
-	
 	
 	public ShopTilYouDrop(int width,int height) {
 		super(width,height);
 	}
 	
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		game = new ShopTilYouDrop(800,700);
 		Thread go = new Thread(game);
