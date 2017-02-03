3147f43e214a75e46870f760f8a3a36b59626b57
Student6 <Student6@10.8.33.107>
2017-02-03 13:08:18 -0500

    dfgdfg

---
+++ ShopTilYouDrop/src/main/ShopTilYouDrop.java
@@ -0,0 +1,31 @@
+package main;
+
+import gui.practice.GUIApplication;
+
+/**
+ * @author Vicki
+ *
+ */
+public class ShopTilYouDrop extends GUIApplication {
+	/**
+	 * static fields
+	 */
+	public static ShopTilYouDrop game;
+	public ShopTilYouDrop(int i, int j) {
+	}
+	
+	@Override
+	protected void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		game = new ShopTilYouDrop(800, 700);
+		Thread go = new Thread(game);
+	}
+
+}
