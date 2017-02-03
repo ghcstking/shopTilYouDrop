2bd964cbe75d501af11af233d895f39418e6e058
Student6 <Student6@10.8.33.107>
2017-02-03 12:41:11 -0500

    created main class in master branch

---
+++ ShopTilYouDrop/src/main/ShopTilYouDrop.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package main;
+
+import gui.practice.GUIApplication;
+
+/**
+ * @author Vicki
+ *
+ */
+public class ShopTilYouDrop extends GUIApplication {
+
+	/**
+	 * 
+	 */
+	public ShopTilYouDrop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
