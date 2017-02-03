eb68ee2a5b5df922e639bf93d4407b103183a835
Student6 <Student6@10.8.33.107>
2017-02-03 13:04:31 -0500

    matched

--- ShopTilYouDrop/src/main/ShopTilYouDrop.java
+++ ShopTilYouDrop/src/main/ShopTilYouDrop.java
@@ -3,34 +3,25 @@
  */
 package main;
 
-import frontEnd.IramWorkerScreen;
-import frontEnd.SammyajitCustomerScreen;
 import gui6.GUIApplication;
-import frontEnd.MatthewMain;
 
 /**
  * @author Vicki
  *
  */
-public class ShopTilYouDrop extends GUIApplication {
-	
+public class ShopTilYouDrop extends gui6.GUIApplication {
 	/**
 	 * static fields
 	 */
 	public static ShopTilYouDrop game;
-	public static MatthewMain main;
-	public static IramWorkerScreen worker;
-	public static SammyajitCustomerScreen customer;
-	
-	public ShopTilYouDrop(int width,int height) {
-		super(width,height);
+	public ShopTilYouDrop(int width, int height) {
+		super(width, height);
 	}
 	
 	public static void main(String[] args) {
-		game = new ShopTilYouDrop(800,700);
+		game = new ShopTilYouDrop(800, 700);
 		Thread go = new Thread(game);
 	}
-
 
 	@Override
 	public void initScreen() {
