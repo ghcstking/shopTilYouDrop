7b4e510157fc1b9f9139453971244852cb492c7f
Student6 <Student6@10.8.33.177>
2017-02-07 13:06:43 -0500

    created methods

--- ShopTilYouDrop/src/customer/RyanCart.java
+++ ShopTilYouDrop/src/customer/RyanCart.java
@@ -1,5 +1,34 @@
 package customer;
 
-public class RyanCart {
+import java.util.ArrayList;
 
+import gui6.components.Visible;
+import gui6.screens.ClickableScreen;
+
+public class RyanCart extends ClickableScreen implements Runnable {
+	
+	public RyanCart(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	private ArrayList<ShoppingCartRyan> itemsInCart;
+	private int numberOfItems;
+
+	public void initAllObjects(ArrayList<Visible> viewObjects){
+		
+	}
+	
+	private void addItem(){
+		
+	}
+	
+	private void removeItem(){
+		
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
