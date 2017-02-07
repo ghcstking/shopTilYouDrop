6189360524738fd5754e78fcc6c388fae97ef6e8
Student 6 <Student 6@10.8.33.165>
2017-02-07 13:15:37 -0500

    ok set screen size later

--- ShopTilYouDrop/src/frontEnd/MatthewMain.java
+++ ShopTilYouDrop/src/frontEnd/MatthewMain.java
@@ -1,15 +1,25 @@
 package frontEnd;
 
+
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 
+import gui6.components.Action;
+import gui6.components.Button;
+import gui6.components.MovingComponent;
+import gui6.components.TextArea;
 import gui6.components.TextLabel;
 import gui6.components.Visible;
 import gui6.screens.ClickableScreen;
 
+
 public class MatthewMain extends ClickableScreen implements Runnable{
 
 	private TextLabel title;
+	private TextArea customer;
+	private TextArea worker;
+
 	public MatthewMain(int width, int height) {
 		super(width, height);
 		Thread screen = new Thread(this);
@@ -24,8 +34,15 @@ public class MatthewMain extends ClickableScreen implements Runnable{
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> arg0) {
-		title = new TextLabel(325,50,300,40,"Shop Til You Drop");
+//		title = new TextLabel(325,50,300,40,"Shop Til You Drop");
+//		viewObjects.add(title);
+
+		title = new TextLabel(20, 200, 500, 40, "This is the title");
+		customer = new TextArea(20, 300, 700, 100, "Customer goes here");
+		worker = new TextArea(20, 300, 700, 100, "Worker goes here");
 		viewObjects.add(title);
+		viewObjects.add(customer);
+		viewObjects.add(worker);
 	}
 	
 	public void update(Graphics2D g) {
--- ShopTilYouDrop/src/main/MatthewShopTilYouDropScreen.java
+++ ShopTilYouDrop/src/main/MatthewShopTilYouDropScreen.java
@@ -1,22 +1,13 @@
 package main;
 
-import java.awt.Color;
 import java.util.ArrayList;
 
-import gui6.components.Action;
-import gui6.components.Button;
-import gui6.components.MovingComponent;
-import gui6.components.TextArea;
-import gui6.components.TextLabel;
 import gui6.components.Visible;
 import gui6.screens.ClickableScreen;
 
 public class MatthewShopTilYouDropScreen extends ClickableScreen implements Runnable{
 	
-	private TextLabel title;
-	private TextArea customer;
-	private TextArea worker;
-
+	
 	public MatthewShopTilYouDropScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -30,12 +21,5 @@ public class MatthewShopTilYouDropScreen extends ClickableScreen implements Runn
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> arg0) {
-		title = new TextLabel(20, 200, 500, 40, "This is the title");
-		customer = new TextArea(20, 300, 700, 100, "Customer goes here");
-		worker = new TextArea(20, 300, 700, 100, "Customer goes here");
-		viewObjects.add(title);
-		viewObjects.add(customer);
-		viewObjects.add(worker);
 	}
-
 }
