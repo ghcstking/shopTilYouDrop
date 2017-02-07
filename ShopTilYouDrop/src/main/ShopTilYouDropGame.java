f42c94933f55bd53b009bc3e23dde733a15981ec
Student6 <Student6@10.8.33.107>
2017-02-07 12:14:33 -0500

    initialized screen

---
+++ ShopTilYouDrop/src/main/ShopTilYouDropGame.java
@@ -0,0 +1,24 @@
+package main;
+
+import gui6.GUIApplication;
+
+public class ShopTilYouDropGame extends GUIApplication {
+
+	public ShopTilYouDropGame(int width, int height) {
+		super(width, height);
+	}
+	
+	public static void main(String[] args) {
+		ShopTilYouDropGame game = new ShopTilYouDropGame(800, 500);
+		Thread app = new Thread(game);
+		app.start();
+	}
+
+
+	@Override
+	public void initScreen() {
+		ShopTilYouDropScreen screen = new ShopTilYouDropScreen(getWidth(), getHeight());
+		setScreen(screen);
+	}
+
+}
---
+++ ShopTilYouDrop/src/main/ShopTilYouDropScreen.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.ArrayList;
+
+import gui6.components.Visible;
+import gui6.screens.ClickableScreen;
+
+public class ShopTilYouDropScreen extends ClickableScreen implements Runnable{
+
+	public ShopTilYouDropScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
