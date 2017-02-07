e1302f59605e41c5b9842f544adea9d23e670899
Student6 <Student6@10.8.33.107>
2017-02-07 12:22:23 -0500

    zoop we need to change the font to arial !!!

--- ShopTilYouDrop/src/main/MatthewShopTilYouDropScreen.java
+++ ShopTilYouDrop/src/main/MatthewShopTilYouDropScreen.java
@@ -1,26 +1,34 @@
 package main;
 
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 
+import gui6.components.TextLabel;
 import gui6.components.Visible;
 import gui6.screens.ClickableScreen;
 
 public class MatthewShopTilYouDropScreen extends ClickableScreen implements Runnable{
 
+	private TextLabel title;
 	public MatthewShopTilYouDropScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		Thread screen = new Thread(this);
+		screen.start();
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> arg0) {
-		// TODO Auto-generated method stub
+		title = new TextLabel(325,50,300,40,"Shop Til You Drop");
+		viewObjects.add(title);
+	}
+	
+	public void update(Graphics2D g) {
 		
 	}
 
