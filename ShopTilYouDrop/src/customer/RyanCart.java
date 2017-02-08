4f95352c7010fd84d346de8c8f0ab15e7bd8e42d
Student6 <Student6@10.8.33.177>
2017-02-08 12:19:28 -0500

    cart functions

--- ShopTilYouDrop/src/customer/RyanCart.java
+++ ShopTilYouDrop/src/customer/RyanCart.java
@@ -10,25 +10,48 @@ import gui6.screens.ClickableScreen;
 
 public class RyanCart extends ClickableGraphic {
 
-	private ArrayList<Component> itemsInCart;
+	private ArrayList<Component> shoppingCart;
 	private int numberOfItems;
 	private TextLabel label;
+	private double timeLeft;
 	
 	public RyanCart(int x, int y) {
-		super(x, y, .5, "resources/sampleImages/cart.png");
-		
+		super(x, y, .5, "resources/sampleImages/cart.png");	
+		shoppingCart = new ArrayList<Component>();
 	}
 
 	private void addItem(){
-		
+		if(isFull(shoppingCart)){
+			
+		}else{
+			shoppingCart.add();
+		}
 	}
 	
 	private void removeItem(){
-		
+		if(isEmpty(shoppingCart)){
+			
+		}else{
+			shoppingCart.remove();
+		}
+	}
+	
+	private boolean isFull(ArrayList cart){
+		if(cart.size() > 10){
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isEmpty(ArrayList cart){
+		if(cart.size() == 0){
+			return true;
+		}
+		return false;
 	}
 
 	public ArrayList<Component> getCartItems() {
-		return itemsInCart;
+		return shoppingCart;
 	}
 	
 }
