a0682d1289c0666688e5d7bf7e121a3ee82464e0
Student6 <Student6@10.8.33.107>
2017-02-10 12:09:57 -0500

    conditions

--- ShopTilYouDrop/src/worker/EdwinRequestGenerator.java
+++ ShopTilYouDrop/src/worker/EdwinRequestGenerator.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class EdwinRequestGenerator implements VickiOrder {
 
 	private static String[] condiments = {"top_bun", "patty", "lettuce", "tomato", "pickles",
-			"cheese", "bottom bun"};
+			"cheese", "bottom_bun"};
 	private static ArrayList<String> request = new ArrayList<String>();
 	
 	@Override
 	public int tip() {
-		return 0;
+		
 	}
 	
 	public static void main(String[] args) {
@@ -21,9 +21,14 @@ public class EdwinRequestGenerator implements VickiOrder {
 		request.add(0, condiments[0]);
 		int numberCondiments = (int) (Math.random()*3)+3;
 		for (int i = 0; i < numberCondiments; i++) {
-			String condiment = condiments[(int)(Math.random()*condiments.length)];
+			String condiment = condiments[(int)(Math.random()*condiments.length-1)];
+			while(request.contains(condiment)) {
+				condiment = condiments[(int)(Math.random()*condiments.length-1)];
+			}
 			request.add(condiment);
 		}
+		request.add(condiments[condiments.length-1]);
+		System.out.println(numberCondiments);
 		for (int i = 0; i < request.size(); i++) {
 			System.out.println(request.get(i));
 		}
