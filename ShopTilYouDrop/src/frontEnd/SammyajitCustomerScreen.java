540a975ac40b93729b8049b0291f729b3a66be3d
Student6 <Student6@10.8.33.172>
2017-02-13 13:05:28 -0500

    ewtgfgdf

--- ShopTilYouDrop/src/frontEnd/SammyajitCustomerScreen.java
+++ ShopTilYouDrop/src/frontEnd/SammyajitCustomerScreen.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import customer.RyanCart;
+import customer.StoreItem;
 import gui6.components.ClickableGraphic;
 import gui6.components.Graphic;
 import gui6.components.TextLabel;
@@ -15,7 +16,7 @@ import gui6.screens.ClickableScreen;
 public class SammyajitCustomerScreen extends ClickableScreen implements Runnable {
 	private TextLabel t;
 	private ClickableGraphic cg;
-	private RyanCart cart;
+	private CartInterface cart;
 	
 	private Graphic g;
 	
@@ -48,6 +49,29 @@ public class SammyajitCustomerScreen extends ClickableScreen implements Runnable
 
 		viewObjects.add(g);
 		//g.setSize();
+		
+
+		viewObjects.add(new ClickSpace(14,114,55,60) {
+			
+			@Override
+			public void act() {
+				System.out.println("Clicked on dress.");
+				cart.addItem(new StoreItem(){
+					
+					@Override
+					public double getPrice() {
+						// TODO Auto-generated method stub
+						return 200.0;
+					}
+					
+					@Override
+					public String getDescription() {
+						// TODO Auto-generated method stub
+						return "dress";
+					}
+				});
+			}
+		});
 	}
  
 }
