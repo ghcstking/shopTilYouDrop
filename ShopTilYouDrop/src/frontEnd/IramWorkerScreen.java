bc9cc7ed75eaaef846c719fa4a25ba81151b268c
Student 8 <Student 8@10.8.33.155>
2017-02-13 12:44:59 -0500

    dasdadd

--- ShopTilYouDrop/src/frontEnd/IramWorkerScreen.java
+++ ShopTilYouDrop/src/frontEnd/IramWorkerScreen.java
@@ -2,6 +2,7 @@ package frontEnd;
 
 import java.util.ArrayList;
 
+import gui6.components.ClickableGraphic;
 import gui6.components.TextLabel;
 import gui6.components.Visible;
 import gui6.screens.ClickableScreen;
@@ -9,11 +10,14 @@ import gui6.screens.ClickableScreen;
 
 public class IramWorkerScreen extends ClickableScreen {
 	private TextLabel title;
+	private ClickableGraphic patty;
 	
 	public IramWorkerScreen(int width, int height) {
 		super(width, height);
 		Thread screen = new Thread(this);
 		screen.start();
+	
+		patty = new ClickableGraphic(50, 50, "resources/patty.PNG");
 	}
 
 	@Override
@@ -25,5 +29,6 @@ public class IramWorkerScreen extends ClickableScreen {
 	public void initAllObjects(ArrayList<Visible> arg0) {
 		title = new TextLabel(325,50,300,40,"Worker");
 		viewObjects.add(title);
+		viewObjects.add(patty);
 	}
 }
