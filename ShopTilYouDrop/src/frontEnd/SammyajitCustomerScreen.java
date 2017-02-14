7697a44da4a5aa85d122c0a38e6c321c8033b10f
Student6 <Student6@10.8.33.172>
2017-02-14 12:33:09 -0500

    worked on coordinates.

--- ShopTilYouDrop/src/frontEnd/SammyajitCustomerScreen.java
+++ ShopTilYouDrop/src/frontEnd/SammyajitCustomerScreen.java
@@ -17,9 +17,9 @@ public class SammyajitCustomerScreen extends ClickableScreen implements Runnable
 	private TextLabel t;
 	private ClickableGraphic cg;
 	private CartInterface cart;
-	
+
 	private Graphic g;
-	
+
 
 	public SammyajitCustomerScreen(int width, int height) {
 		super(width, height);
@@ -29,49 +29,110 @@ public class SammyajitCustomerScreen extends ClickableScreen implements Runnable
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> arg0) {
 
 		t = new TextLabel(10, 50, 500, 40, "You are a customer. Pick items to put in cart.");
-		cart= new RyanCart(300,50);
+		//		cart= new RyanCart(300,50);
 
 		viewObjects.add(t);
-		
+
 
 		g=new Graphic(5,100,.82,"resources/CustomerBackground.png");
 
-		
+
 
 		viewObjects.add(g);
 		//g.setSize();
-		
+
 
 		viewObjects.add(new ClickSpace(14,114,55,60) {
-			
+
 			@Override
 			public void act() {
 				System.out.println("Clicked on dress.");
-				cart.addItem(new StoreItem(){
-					
-					@Override
-					public double getPrice() {
-						// TODO Auto-generated method stub
-						return 200.0;
-					}
-					
-					@Override
-					public String getDescription() {
-						// TODO Auto-generated method stub
-						return "dress";
-					}
-				});
+				//				addToCart("dress",200.0);
+
+			}
+
+		});
+
+		viewObjects.add(new ClickSpace(83,115,40,30) {
+
+			@Override
+			public void act() {
+				System.out.println("Clicked on shoes.");
+				//				addToCart("dress",200.0);
+
+			}
+
+		});
+
+
+
+		viewObjects.add(new ClickSpace(155,110,115,40) {
+
+			@Override
+			public void act() {
+				System.out.println("Clicked on tVs.");
+				//			addToCart("dress",200.0);
+
+			}
+
+		});
+
+		viewObjects.add(new ClickSpace(285,110,115,40) {
+
+			@Override
+			public void act() {
+				System.out.println("Clicked on TVs.");
+				//			addToCart("dress",200.0);
+
+			}
+
+		});
+		
+		viewObjects.add(new ClickSpace(18,185,40,40) {
+
+			@Override
+			public void act() {
+				System.out.println("Clicked on CD.");
+				//			addToCart("dress",200.0);
+
+			}
+
+		});
+		
+		viewObjects.add(new ClickSpace(150,185,40,40) {
+
+			@Override
+			public void act() {
+				System.out.println("Clicked on books.");
+				//			addToCart("dress",200.0);
+
+			}
+
+		});
+	}
+	private void addToCart(String string, double d) {
+		cart.addItem(new StoreItem(){
+
+			@Override
+			public double getPrice() {
+				// TODO Auto-generated method stub
+				return d;
+			}
+
+			@Override
+			public String getDescription() {
+				// TODO Auto-generated method stub
+				return string;
 			}
 		});
 	}
- 
 }
