6aea03fde5a9e9b9fbe857b76e5bde3fb225b1c1
Student 6 <Student 6@10.8.33.173>
2017-02-15 12:36:49 -0500

    a new hope

--- ShopTilYouDrop/src/customer/DanielFreeMoney.java
+++ ShopTilYouDrop/src/customer/DanielFreeMoney.java
@@ -8,7 +8,7 @@ public class DanielFreeMoney {
 
 	private static ArrayList<Money> moneyAvailable;
 	private static int[] values;
-
+	private static SammyajitCustomerScreen game;
 	public static void main(String[] args){
 
 		placeMoney();
@@ -16,21 +16,23 @@ public class DanielFreeMoney {
 	}
 
 	public static void placeMoney(){
+		game = new SammyajitCustomerScreen(100, 100);
 		moneyAvailable = new ArrayList<Money>();
 		int[] values = {5,10,20,50,100};
 		int time = 5;
 
-		//		int x = (int) ((double)screen.getWidth() * rand);
-		//		int y = (int) ((double)screen.getHeight() * rand);
-
+				
 		while(time >0){
 			try {
 				double rand = Math.random();
+				double rand1 = Math.random();
+				int x = (int) ((double)game.getWidth() * rand);
+				int y = (int) ((double)game.getHeight() * rand1);
 				int newValue = (int) (5 * rand);
-				Money newMon = new Money( 0, 0, 10, 10, "coin.png");		
+				Money newMon = new Money( x, y, 10, 10, "coin.png");		
 				newMon.setValue(values[newValue]);
 				moneyAvailable.add(newMon);
-				System.out.println(moneyAvailable.get(0).getValue());
+				System.out.println(" X: " + moneyAvailable.get(0).getX() + " Y:  " + moneyAvailable.get(0).getY()+" Value: " + moneyAvailable.get(0).getValue());
 				Thread.sleep((long)(2000));
 
 				moneyAvailable.remove(0);
