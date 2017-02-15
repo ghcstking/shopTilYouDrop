acb1319e61ebd81aed7ce036f30f60175168dba6
Student 6 <Student 6@10.8.33.173>
2017-02-15 12:17:46 -0500

    hope

--- ShopTilYouDrop/src/customer/DanielFreeMoney.java
+++ ShopTilYouDrop/src/customer/DanielFreeMoney.java
@@ -10,32 +10,43 @@ public class DanielFreeMoney {
 	private static int[] values;
 
 	public static void main(String[] args){
+
+		placeMoney();
+
+	}
+
+	public static void placeMoney(){
+		moneyAvailable = new ArrayList<Money>();
 		int[] values = {5,10,20,50,100};
+		int time = 5;
 
-	}
+		//		int x = (int) ((double)screen.getWidth() * rand);
+		//		int y = (int) ((double)screen.getHeight() * rand);
 
-	public static void placeMoney(SammyajitCustomerScreen screen){
-		double rand = Math.random();
-		int newValue = (int) (values.length * rand);
-		int x = (int) ((double)screen.getWidth() * rand);
-		int y = (int) ((double)screen.getHeight() * rand);
+		while(time >0){
+			try {
+				double rand = Math.random();
+				int newValue = (int) (5 * rand);
+				Money newMon = new Money( 0, 0, 10, 10, "coin.png");		
+				newMon.setValue(values[newValue]);
+				moneyAvailable.add(newMon);
+				System.out.println(moneyAvailable.get(0).getValue());
+				Thread.sleep((long)(2000));
 
-		try {
-			Money newMon = new Money( x, y, 10, 10, "coin.png");		
-			newMon.setValue(values[newValue]);
-			moneyAvailable.add(newMon);
-			Thread.sleep((long)(2000));
-			moneyAvailable.remove(0);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
+				moneyAvailable.remove(0);
+				time--;
+			} catch (InterruptedException e) { 
+				e.printStackTrace();
+			}
+
 		}
-		
-	 
-		
-	
+
+
+
+
 
 	}
 
-	
-	
+
+
 }
