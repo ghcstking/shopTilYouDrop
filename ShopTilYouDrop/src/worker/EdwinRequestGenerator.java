108402cb709b369a5ccdfc47d56c78dc89fae711
Student6 <Student6@10.8.33.107>
2017-02-15 12:19:03 -0500

    lol help

--- ShopTilYouDrop/src/worker/EdwinRequestGenerator.java
+++ ShopTilYouDrop/src/worker/EdwinRequestGenerator.java
@@ -18,36 +18,8 @@ public class EdwinRequestGenerator implements VickiOrder {
 		return tip;
 	}
 	
-	private static double price(ArrayList<String> request) {
-		double price = 0;
-		for (int i = 0; i < request.size(); i++) {
-			price += pricesArray[java.util.Arrays.asList(condiments).indexOf(request.get(i))];
-		}
-		return ;
-	}
-	
-
-//	public void generate(WorkerInterface w) {
-//		ArrayList<String> request = new ArrayList<String>();
-//		request.add(0, condiments[0]);
-//		int numberCondiments = (int) (Math.random()*3)+3;
-//		for (int i = 0; i < numberCondiments; i++) {
-//			String condiment = condiments[(int)(Math.random()*condiments.length)];
-//			while(condiment == "top_bun" || condiment == "bottom_bun"){
-//				condiment = condiments[(int)(Math.random()*condiments.length)];
-//			}
-//			request.add(condiment);
-//		}
-//		request.add(condiments[condiments.length-1]);
-//		w.displayNewRequest(request);
-//		price(request);
-//	}
-	
-	public static void main(String[] args) {
-		generate();
-	}
-	
-	public static void generate() {
+	@Override
+	public void generate(WorkerInterface w) {
 		ArrayList<String> request = new ArrayList<String>();
 		request.add(0, condiments[0]);
 		int numberCondiments = (int) (Math.random()*3)+3;
@@ -59,10 +31,29 @@ public class EdwinRequestGenerator implements VickiOrder {
 			request.add(condiment);
 		}
 		request.add(condiments[condiments.length-1]);
-		//w.displayNewRequest(request);
-		System.out.println(price(request));
-		for (int i = 0; i < request.size(); i++) {
-			System.out.println(request.get(i));
-		}
+		w.displayNewRequest(request);
 	}
+	
+//	public static void main(String[] args) {
+//		generate();
+//	}
+//	
+//	public static void generate() {
+//		ArrayList<String> request = new ArrayList<String>();
+//		request.add(0, condiments[0]);
+//		int numberCondiments = (int) (Math.random()*3)+3;
+//		for (int i = 0; i < numberCondiments; i++) {
+//			String condiment = condiments[(int)(Math.random()*condiments.length)];
+//			while(condiment == "top_bun" || condiment == "bottom_bun"){
+//				condiment = condiments[(int)(Math.random()*condiments.length)];
+//			}
+//			request.add(condiment);
+//		}
+//		request.add(condiments[condiments.length-1]);
+//		//w.displayNewRequest(request);
+//		//System.out.println(price(request));
+//		for (int i = 0; i < request.size(); i++) {
+//			System.out.println(request.get(i));
+//		}
+//	}
 }
