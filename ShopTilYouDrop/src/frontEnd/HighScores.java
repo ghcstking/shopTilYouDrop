9be61b0d9090b1cc190ac20167db0821435f0981
Student 6 <Student 6@10.8.33.165>
2017-02-27 12:30:37 -0500

    created arrayList, next create scores, display

--- ShopTilYouDrop/src/frontEnd/HighScores.java
+++ ShopTilYouDrop/src/frontEnd/HighScores.java
@@ -1,11 +1,40 @@
 package frontEnd;
 
-public interface HighScores {
+import gui6.components.TextLabel;
 
-	public void getScore();
+public class HighScores extends TextLabel{
+	
+	private static String username;
+	private static int score;
+	private static int rank;
 
-	public boolean isHovered(int x, int y);
+	public HighScores(int x, int y, int w, int h, String text) {
+		super(x, y, w, h, text);
+		// TODO Auto-generated constructor stub
+	}
 
-	public void act();
+	public static String getUsername() {
+		return username;
+	}
+
+	public static void setUsername(String username) {
+		HighScores.username = username;
+	}
+
+	public static int getScore() {
+		return score;
+	}
+
+	public static void setScore(int score) {
+		HighScores.score = score;
+	}
+
+	public static int getRank() {
+		return rank;
+	}
+
+	public static void setRank(int rank) {
+		HighScores.rank = rank;
+	}
 
 }
--- ShopTilYouDrop/src/frontEnd/MatthewAddScore.java
+++ ShopTilYouDrop/src/frontEnd/MatthewAddScore.java
@@ -1,67 +1,19 @@
 package frontEnd;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import gui6.components.Visible;
-import gui6.screens.ClickableScreen;
+public class MatthewAddScore{
 
-public abstract class MatthewAddScore extends ClickableScreen{
-
-	public MatthewAddScore(int width, int height) {
-		super(width, height);
-		// TODO Auto-generated constructor stub
-	}
-
-	private static ArrayList<HighScores> playerScores;
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	public void placeScores(ArrayList<HighScores> viewObjects) {
-		allScores(viewObjects);
-		playerScores = new ArrayList<HighScores>();
-		for(HighScores i: playerScores){
-			//size of ArrayList can be accessed using size()
-			//element of ArrayList at index i is get(i)
-			if(i instanceof HighScores){
-				playerScores.add((HighScores)i);
-			}
-		}
-	}
-
-	public abstract void allScores(ArrayList<HighScores> lst);
-
-	public MouseListener getMouseListener(){
-		return this;
-	}
-
-	public void mouseClicked(MouseEvent e) {
-		for(HighScores i: playerScores){
-			if(i.isHovered(e.getX(), e.getY())){
-				i.act();
-			}
-		}
-	}
+	private static ArrayList<HighScores> scores;
+	private static int crank;
+	private static String user;
+	//place highscore screen here private static MatthewHighScoreScreen gameover;
 	
-	public void addObject(Visible v){
-		super.addObjects(v);
-		if(v instanceof HighScores){
-			playerScores.add((HighScores)v);
-		}
+	public static void main(String[] args){
+		placeScores();
 	}
 
-
-
-	public void remove(Visible v){
-		super.remove(v);
-		playerScores.remove((HighScores)v);
-	} 
-
+	public static void placeScores() {
+		//gameover = new 
+	}
 }
