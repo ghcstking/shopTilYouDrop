795365e42f4b276d06e840c65ad1efa5c427b9d1
Student6 <Student6@10.8.33.105>
2017-03-02 12:07:44 -0500

    removeitem

--- ShopTilYouDrop/src/frontEnd/CartInterface.java
+++ ShopTilYouDrop/src/frontEnd/CartInterface.java
@@ -6,6 +6,8 @@ import customer.StoreItem;
 
 public interface CartInterface {
 	void addItem(StoreItem i);
+
+	void removeItem(StoreItem i);
 	
 	//public ArrayList<StoreItem> getPurchasedItems();
 }
