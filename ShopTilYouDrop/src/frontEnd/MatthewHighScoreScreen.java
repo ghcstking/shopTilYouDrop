adda331b9f39486c9e51e3a3633e5beb657fb2ed
Student 6 <Student 6@10.8.33.165>
2017-03-02 13:15:57 -0500

    done

--- ShopTilYouDrop/src/frontEnd/MatthewHighScoreScreen.java
+++ ShopTilYouDrop/src/frontEnd/MatthewHighScoreScreen.java
@@ -1,6 +1,5 @@
 package frontEnd;
 
-import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -12,11 +11,10 @@ import gui6.screens.ClickableScreen;
 public class MatthewHighScoreScreen extends ClickableScreen implements Runnable, KeyListener{
 
 	private ArrayList <HighScoreInterface> scores;
-	private TextLabelScore rank;
-	private int y= 200;
+	private TextLabel rank;
+	private int y;
 	private static String user;
 	private static TextLabel title;
-	private static TextLabel username;
 	
 	public MatthewHighScoreScreen(int width, int height) {
 		super(width, height);
@@ -51,12 +49,12 @@ public class MatthewHighScoreScreen extends ClickableScreen implements Runnable,
 	@Override
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
 		title = new TextLabel(300, 50, 500, 40, "High Scores");
-		title.setSize(30);
 		viewObjects.add(title);
 		scores = new ArrayList<HighScoreInterface>();
+		scores.add(new Player("",0.0));
 		int allscores = scores.size();
+		y = 100;
 		for(int i = 0; i < allscores; i++){
-			scores.add(new Player("",0.0));
 //			for(int k = 0; k< allscores; i++){
 //				if(scores.get(k).getScore() < scores.get(k+1).getScore()){
 //					HighScoreInterface tmp = scores.get(k);
@@ -64,13 +62,11 @@ public class MatthewHighScoreScreen extends ClickableScreen implements Runnable,
 //					scores.set(k+1, tmp);
 //				}
 //			}
-			scores.get(0).setUsername("User"+0);
-			scores.get(0).setScore(5.4);
-			//scores add score is set in vicki's thing list.set(index, Integer.valueof(9))
-			rank = new TextLabelScore(000, y, 600, 50, 0+"   |   "+scores.get(0).getUsername()+"   |   "+scores.get(0).getScore());
+			scores.get(i).setUsername("User"+i);
+			scores.get(i).setScore(5.4);
+			rank = new TextLabel(100, y, 600, 50, (0+1)+"            |            "+scores.get(0).getUsername()+"            |            "+scores.get(0).getScore());
 			viewObjects.add(rank);
 			y+=50;
 		}
-		viewObjects.add(title);
 	}
 }
